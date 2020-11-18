872d06b3bc89b5f939e35b1307f0f0bde5fd7260
Kjell <kjell@holmgren.com>
2020-11-18 18:05:12 -0500

    initial

--- liiklus-sink/src/main/java/io/kettil/fn/liiklus/Sink.java
+++ liiklus-sink/src/main/java/io/kettil/fn/liiklus/Sink.java
@@ -1,12 +1,26 @@
 package io.kettil.fn.liiklus;
 
+import com.github.bsideup.liiklus.protocol.*;
+import io.grpc.ManagedChannel;
+import io.grpc.netty.NettyChannelBuilder;
 import lombok.Data;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
 import picocli.CommandLine;
+import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+import java.util.function.Function;
+
+@Slf4j
 @Data
 public class Sink implements Runnable {
     @CommandLine.Option(names = {"-h", "--help"}, usageHelp = true, description = "display a help message")
     private boolean helpRequested = false;
+
+    String host = "localhost";
 
     @CommandLine.Option(names = {"-p", "--port"}, description = "liiklus port", defaultValue = "6565")
     private int port;
@@ -21,8 +35,74 @@ public class Sink implements Runnable {
         new CommandLine(new Sink()).execute(args);
     }
 
+    private final CountDownLatch latch = new CountDownLatch(1);
+
+    @SneakyThrows
     public void run() {
-        System.out.println(this.getTopic());
-        System.out.println("XXXX: " + this);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("shutting down");
+            latch.countDown();
+        }));
+
+        ManagedChannel channel = NettyChannelBuilder.forTarget(host + ":" + port)
+                .directExecutor()
+                .usePlaintext()
+                .build();
+
+        log.info("opened channel");
+
+        SubscribeRequest subscribeAction = SubscribeRequest.newBuilder()
+                .setTopic(topic)
+                .setGroup(group)
+                .setAutoOffsetReset(SubscribeRequest.AutoOffsetReset.EARLIEST)
+                .build();
+
+        ReactorLiiklusServiceGrpc.ReactorLiiklusServiceStub stub = ReactorLiiklusServiceGrpc.newReactorStub(channel);
+
+        log.info("created service stub");
+
+        // Consume the events
+        Function<Integer, Function<ReceiveReply.Record, Publisher<?>>> businessLogic = partition -> record -> {
+            log.info("Processing record from partition {} offset {}", partition, record.getOffset());
+
+            // simulate processing
+            return Mono.delay(Duration.ofMillis(200));
+        };
+
+        stub
+                .subscribe(subscribeAction)
+                .filter(it -> it.getReplyCase() == SubscribeReply.ReplyCase.ASSIGNMENT)
+                .map(SubscribeReply::getAssignment)
+                .doOnNext(assignment -> log.info("Assigned to partition {}", assignment.getPartition()))
+                .flatMap(assignment -> stub
+                        // Start receiving the events from a partition
+                        .receive(ReceiveRequest.newBuilder().setAssignment(assignment).build())
+                        .window(2) // ACK every nth record
+                        .concatMap(
+                                batch -> batch
+                                        .map(ReceiveReply::getRecord)
+                                        .delayUntil(businessLogic.apply(assignment.getPartition()))
+                                        .sample(Duration.ofSeconds(5)) // ACK every 5 seconds
+                                        .onBackpressureLatest()
+                                        .delayUntil(record -> {
+                                            log.info("ACKing partition {} offset {}", assignment.getPartition(), record.getOffset());
+                                            return stub.ack(
+                                                    AckRequest.newBuilder()
+                                                            .setTopic(subscribeAction.getTopic())
+                                                            .setGroup(subscribeAction.getGroup())
+                                                            .setGroupVersion(subscribeAction.getGroupVersion())
+                                                            .setPartition(assignment.getPartition())
+                                                            .setOffset(record.getOffset())
+                                                            .build()
+                                            );
+                                        }),
+                                1
+                        )
+                )
+                .blockLast();
+
+        log.info("complete");
+
+        latch.await();
     }
 }
--- liiklus-source/src/main/java/io/kettil/fn/liiklus/Source.java
+++ liiklus-source/src/main/java/io/kettil/fn/liiklus/Source.java
@@ -1,15 +1,31 @@
 package io.kettil.fn.liiklus;
 
+import com.github.bsideup.liiklus.protocol.LiiklusEvent;
+import com.github.bsideup.liiklus.protocol.PublishRequest;
+import com.github.bsideup.liiklus.protocol.ReactorLiiklusServiceGrpc;
+import com.github.bsideup.liiklus.protocol.SubscribeRequest;
+import com.google.protobuf.ByteString;
+import io.grpc.ManagedChannel;
+import io.grpc.netty.NettyChannelBuilder;
 import lombok.Data;
-import lombok.ToString;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+
+@Slf4j
 @Data
 public class Source implements Runnable {
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "display a help message")
     private boolean helpRequested = false;
+
+    private String host = "localhost";
 
     @Option(names = {"-p", "--port"}, description = "liiklus port", defaultValue = "6565")
     private int port;
@@ -24,8 +40,52 @@ public class Source implements Runnable {
         new CommandLine(new Source()).execute(args);
     }
 
+    private final CountDownLatch latch = new CountDownLatch(1);
+
+    @SneakyThrows
     public void run() {
-        System.out.println(this.getTopic());
-        System.out.println("XXXX: " + this);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("shutting down");
+            latch.countDown();
+        }));
+
+        ManagedChannel channel = NettyChannelBuilder.forTarget(host + ":" + port)
+                .directExecutor()
+                .usePlaintext()
+                .build();
+
+        log.info("opened channel");
+
+        SubscribeRequest subscribeAction = SubscribeRequest.newBuilder()
+                .setTopic(topic)
+                .setGroup(group)
+                .setAutoOffsetReset(SubscribeRequest.AutoOffsetReset.EARLIEST)
+                .build();
+
+        ReactorLiiklusServiceGrpc.ReactorLiiklusServiceStub stub = ReactorLiiklusServiceGrpc.newReactorStub(channel);
+
+        log.info("created service stub");
+
+        Flux.interval(Duration.ofSeconds(1))
+                .onBackpressureDrop()
+                .concatMap(it -> stub.publish(
+                        PublishRequest.newBuilder()
+                                .setTopic(subscribeAction.getTopic())
+                                .setKey(ByteString.copyFromUtf8(UUID.randomUUID().toString()))
+                                .setLiiklusEvent(
+                                        LiiklusEvent.newBuilder()
+                                                .setId(UUID.randomUUID().toString())
+                                                .setType("com.example.event")
+                                                .setSource("/example")
+                                                .setDataContentType("text/plain")
+                                                .setData(ByteString.copyFromUtf8(UUID.randomUUID().toString()))
+                                )
+                                .build()
+                ))
+                .subscribe();
+
+        log.info("subscribed to topic in group");
+
+        latch.await();
     }
 }
